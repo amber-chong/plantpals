5c68b3c2007b82b1299d488d2ef7c44412832c8b
amber-chong <amchong05@gmail.com>
2024-04-28 17:53:06 +1000

    its a map! doesnt work though

--- src/app/map/map.page.ts
+++ src/app/map/map.page.ts
@@ -1,14 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
-
+import { IonicModule } from '@ionic/angular';
 @Component({
   selector: 'app-map',
   templateUrl: './map.page.html',
   styleUrls: ['./map.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [CommonModule, FormsModule, IonicModule]
 })
 export class MapPage implements OnInit {
 
